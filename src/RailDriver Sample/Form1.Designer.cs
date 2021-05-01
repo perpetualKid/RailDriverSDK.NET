9a59db0fbdb439941120411a3f7b3e257effc9f5
perpetualKid <perpetualKid@azuros.net>
2021-05-01 13:23:33 +0200

    Adding icon files

--- src/RailDriver Sample/Form1.Designer.cs
+++ src/RailDriver Sample/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.BtnEnumerate = new System.Windows.Forms.Button();
             this.CboDevices = new System.Windows.Forms.ComboBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
@@ -52,10 +53,9 @@
             // 
             // BtnEnumerate
             // 
-            this.BtnEnumerate.Location = new System.Drawing.Point(9, 17);
-            this.BtnEnumerate.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnEnumerate.Location = new System.Drawing.Point(12, 26);
             this.BtnEnumerate.Name = "BtnEnumerate";
-            this.BtnEnumerate.Size = new System.Drawing.Size(98, 20);
+            this.BtnEnumerate.Size = new System.Drawing.Size(131, 31);
             this.BtnEnumerate.TabIndex = 0;
             this.BtnEnumerate.Text = "Enumerate";
             this.BtnEnumerate.UseVisualStyleBackColor = true;
@@ -64,45 +64,44 @@
             // CboDevices
             // 
             this.CboDevices.FormattingEnabled = true;
-            this.CboDevices.Location = new System.Drawing.Point(134, 16);
-            this.CboDevices.Margin = new System.Windows.Forms.Padding(2);
+            this.CboDevices.Location = new System.Drawing.Point(179, 25);
             this.CboDevices.Name = "CboDevices";
-            this.CboDevices.Size = new System.Drawing.Size(230, 21);
+            this.CboDevices.Size = new System.Drawing.Size(305, 28);
             this.CboDevices.TabIndex = 1;
             this.CboDevices.SelectedIndexChanged += new System.EventHandler(this.CboDevices_SelectedIndexChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(9, 104);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 160);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(492, 69);
+            this.listBox1.Size = new System.Drawing.Size(655, 104);
             this.listBox1.TabIndex = 6;
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 342);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 534);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(521, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(695, 26);
             this.statusStrip1.TabIndex = 9;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
             // BtnCallback
             // 
-            this.BtnCallback.Location = new System.Drawing.Point(9, 64);
-            this.BtnCallback.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnCallback.Location = new System.Drawing.Point(12, 98);
             this.BtnCallback.Name = "BtnCallback";
-            this.BtnCallback.Size = new System.Drawing.Size(131, 20);
+            this.BtnCallback.Size = new System.Drawing.Size(175, 31);
             this.BtnCallback.TabIndex = 10;
             this.BtnCallback.Text = "Setup for Callback";
             this.BtnCallback.UseVisualStyleBackColor = true;
@@ -111,39 +110,35 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 1);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.Size = new System.Drawing.Size(100, 20);
             this.label1.TabIndex = 16;
             this.label1.Text = "1. Do this first";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 48);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.Size = new System.Drawing.Size(257, 20);
             this.label2.TabIndex = 17;
             this.label2.Text = "2. Set for data callback and read data";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 188);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 289);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.Size = new System.Drawing.Size(131, 20);
             this.label3.TabIndex = 18;
             this.label3.Text = "3. Write to Display";
             // 
             // BtnClear
             // 
-            this.BtnClear.Location = new System.Drawing.Point(414, 76);
-            this.BtnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnClear.Location = new System.Drawing.Point(552, 117);
             this.BtnClear.Name = "BtnClear";
-            this.BtnClear.Size = new System.Drawing.Size(86, 24);
+            this.BtnClear.Size = new System.Drawing.Size(115, 37);
             this.BtnClear.TabIndex = 20;
             this.BtnClear.Text = "Clear Listbox";
             this.BtnClear.UseVisualStyleBackColor = true;
@@ -151,37 +146,33 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(112, 204);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(149, 314);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.Size = new System.Drawing.Size(61, 27);
             this.textBox1.TabIndex = 21;
             this.textBox1.Text = "110";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(62, 204);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(83, 314);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.Size = new System.Drawing.Size(61, 27);
             this.textBox2.TabIndex = 22;
             this.textBox2.Text = "121";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 204);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Location = new System.Drawing.Point(16, 314);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(47, 20);
+            this.textBox3.Size = new System.Drawing.Size(61, 27);
             this.textBox3.TabIndex = 23;
             this.textBox3.Text = "118";
             // 
             // BtnWriteDisplay
             // 
-            this.BtnWriteDisplay.Location = new System.Drawing.Point(163, 203);
-            this.BtnWriteDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnWriteDisplay.Location = new System.Drawing.Point(217, 312);
             this.BtnWriteDisplay.Name = "BtnWriteDisplay";
-            this.BtnWriteDisplay.Size = new System.Drawing.Size(103, 20);
+            this.BtnWriteDisplay.Size = new System.Drawing.Size(137, 31);
             this.BtnWriteDisplay.TabIndex = 24;
             this.BtnWriteDisplay.Text = "Write To Display";
             this.BtnWriteDisplay.UseVisualStyleBackColor = true;
@@ -189,10 +180,9 @@
             // 
             // BtnSpeakerOn
             // 
-            this.BtnSpeakerOn.Location = new System.Drawing.Point(12, 271);
-            this.BtnSpeakerOn.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnSpeakerOn.Location = new System.Drawing.Point(16, 417);
             this.BtnSpeakerOn.Name = "BtnSpeakerOn";
-            this.BtnSpeakerOn.Size = new System.Drawing.Size(103, 20);
+            this.BtnSpeakerOn.Size = new System.Drawing.Size(137, 31);
             this.BtnSpeakerOn.TabIndex = 25;
             this.BtnSpeakerOn.Text = "Speaker On";
             this.BtnSpeakerOn.UseVisualStyleBackColor = true;
@@ -200,10 +190,9 @@
             // 
             // BtnSpeakerOff
             // 
-            this.BtnSpeakerOff.Location = new System.Drawing.Point(119, 271);
-            this.BtnSpeakerOff.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnSpeakerOff.Location = new System.Drawing.Point(159, 417);
             this.BtnSpeakerOff.Name = "BtnSpeakerOff";
-            this.BtnSpeakerOff.Size = new System.Drawing.Size(103, 20);
+            this.BtnSpeakerOff.Size = new System.Drawing.Size(137, 31);
             this.BtnSpeakerOff.TabIndex = 26;
             this.BtnSpeakerOff.Text = "Speaker Off";
             this.BtnSpeakerOff.UseVisualStyleBackColor = true;
@@ -212,10 +201,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 255);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(13, 392);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.Size = new System.Drawing.Size(176, 20);
             this.label4.TabIndex = 27;
             this.label4.Text = "4. Turn Speaker On or Off";
             // 
@@ -226,10 +214,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(521, 364);
+            this.ClientSize = new System.Drawing.Size(695, 560);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.BtnSpeakerOff);
             this.Controls.Add(this.BtnSpeakerOn);
@@ -246,11 +234,11 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.CboDevices);
             this.Controls.Add(this.BtnEnumerate);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "C# RailDriver Sample";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
